8d6841f5f4162257d84a83e135beeaef15c8e788
mcheich <mcheich@gmail.com>
2020-04-21 21:44:50 -0400

    smoke test passing

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn YoMama
         </a>
       </header>
     </div>
